f9d378679fe23edc4623a3bb5660f7999483b109
Vulture2009 <73707254+Vulture2009@users.noreply.github.com>
2021-05-04 21:49:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,6 +27,12 @@ scoreRightWrist = 0;
 
 game_status = "";
 
+function preload()
+{
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  Miss = loadSound(missed.wav);
+}
+
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent('canvas');
@@ -92,7 +98,7 @@ function draw(){
       fill(250,0,0);
        stroke(0,0,250);
        strokeWeight(0.5);
-      paddle1Y = mouseY; 
+      paddle1Y =rightWristY; 
       rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
       
       
@@ -166,9 +172,11 @@ function move(){
   if (ball.x-2.5*ball.r/2< 0){
   if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
     ball.dx = -ball.dx+0.5; 
+    ball_touch_paddel.play();
   }
   else{
     pcscore++;
+    missed.play();
     reset();
     navigator.vibrate(100);
   }
@@ -181,7 +189,7 @@ if(pcscore ==4){
     stroke("white");
     textSize(25)
     text("Game Over!☹☹",width/2,height/2);
-    text("Reload The Page!",width/2,height/2+30)
+    text("Press Restart to play again!!",width/2,height/2+30)
     noLoop();
     pcscore = 0;
 }
@@ -210,4 +218,10 @@ function paddleInCanvas(){
   if(mouseY < 0){
     mouseY =0;
   }  
+}
+
+function restart()
+{
+  pcscore = 0;
+  loop();
 }
